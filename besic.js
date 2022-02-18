8356df5c3021c2ef9a690403ce0bfe707137f9ce
noormohammad20 <mdsaim204@gmail.com>
2022-02-18 20:49:36 +0600

    besic learn form js

---
+++ besic.js
@@ -0,0 +1,63 @@
+// variable
+var deposit = 400
+// condition
+if (deposit > 500) {
+
+}
+else if (deposit < 200) {
+
+}
+else if (deposit == 500) {
+
+}
+else if (deposit != 500) {
+
+}
+else if (deposit >= 500) {
+
+}
+else if (deposit <= 500) {
+
+}
+else {
+
+}
+
+// array 
+const numbers = [23, 52, 66, 74, 77, 23, 64, 77, 44,]
+const numberCount = numbers.length;
+numbers.push(111)
+numbers.pop()
+numbers[2] = 514;
+// check whether 222 included in the array 
+if (numbers.indexOf(222) != -1) {
+
+}
+if (numbers.includes(222)) {
+
+}
+
+// while loop and for loop and for of loop
+
+for (const number of numbers) {
+
+}
+
+// function
+
+function getFullName(first, second) {
+    const name = first + '' + second;
+    return name
+}
+const person = getFullName('abdur', 'rohim')
+
+// Object 
+const bottle = {
+    color: 'yellow',
+    contain: 'water',
+    price: 50,
+
+}
+
+
+
